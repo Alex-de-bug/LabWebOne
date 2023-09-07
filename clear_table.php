f94f7f9d0ce06423b669d738de0ace8fcf31b97b
Alex-de-bug <113011463+Alex-de-bug@users.noreply.github.com>
2023-09-08 00:17:52 +0300

    add clear table on server

---
+++ clear_table.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$port = "5432";
+$dbname = "web";
+$user = "postgres";
+$password = "123говна";
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+if (!$conn) {
+    die("Ошибка подключения к базе данных: " . pg_last_error());
+}
+$query = "DELETE FROM hits";
+$result = pg_query($conn, $query);
+if (!$result) {
+    die("Ошибка выполнения запроса: " . pg_last_error());
+}
+
+echo include 'database.php';
